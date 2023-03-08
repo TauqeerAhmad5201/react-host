3194248ee6bd45b3adea06d9e161504ff8869ce8
TauqeerAhmad5201 <hellotauqeer@gmail.com>
2023-03-08 23:52:57 +0530

    added DescriptionList.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Blog from './components/Blog'
 import './App.css';
+import DescriptionList from './components/DescriptionList';
 
 function App() {
   return (
     <>
     <Blog></Blog>
+    <DescriptionList></DescriptionList>
     </>
   );
 }
