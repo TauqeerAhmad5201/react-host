a14c9ea19513823c990a636ea61e15c7d0b5e538
TauqeerAhmad5201 <hellotauqeer@gmail.com>
2023-03-08 23:53:37 +0530

    solved ul role="list"

--- src/components/DescriptionList.js
+++ src/components/DescriptionList.js
@@ -37,7 +37,7 @@ export default function DescriptionList() {
           <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
             <dt className="text-sm font-medium text-gray-500">Attachments</dt>
             <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
-              <ul role="list" className="divide-y divide-gray-200 rounded-md border border-gray-200">
+              <ul className="divide-y divide-gray-200 rounded-md border border-gray-200">
                 <li className="flex items-center justify-between py-3 pl-3 pr-4 text-sm">
                   <div className="flex w-0 flex-1 items-center">
                     <PaperClipIcon className="h-5 w-5 flex-shrink-0 text-gray-400" aria-hidden="true" />
