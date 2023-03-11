c0d248a8e479284cb7e39145607d13a33853dcc6
TauqeerAhmad5201 <hellotauqeer@gmail.com>
2023-03-11 18:50:57 +0530

    added Darkmode app to App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Blog from './components/Blog'
 import './App.css';
 import DescriptionList from './components/DescriptionList';
+import Darkmode from './components/Darkmode';
 
 function App() {
   return (
     <>
     <Blog></Blog>
     <DescriptionList></DescriptionList>
+    <Darkmode></Darkmode>
     </>
   );
 }
