6f8539bf6f39acba18c110c43d0ea4aaf1c93bac
TauqeerAhmad5201 <hellotauqeer@gmail.com>
2023-03-12 00:40:22 +0530

    changed button type to access button

--- src/components/Darkmode.js
+++ src/components/Darkmode.js
@@ -249,7 +249,7 @@ export default function Darkmode() {
                 </div>
                 <div className="bg-gray-50 px-4 py-3 text-right sm:px-6">
                   <button
-                    type="submit"
+                    type="button"
                     className="inline-flex justify-center rounded-md bg-indigo-600 py-2 px-3 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
                   >
                     Save
