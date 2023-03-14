81a3e8c0f730e3a81fc7b06751338116dbf49be3
TauqeerAhmad5201 <hellotauqeer@gmail.com>
2023-03-14 23:56:13 +0530

    added banner app to Appp.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import Blog from './components/Blog'
 import './App.css';
 import DescriptionList from './components/DescriptionList';
 import Darkmode from './components/Darkmode';
+import Banner from './components/Banner'
 
 function App() {
   return (
     <>
+    <Banner></Banner>
     <Blog></Blog>
     <DescriptionList></DescriptionList>
     <Darkmode></Darkmode>
