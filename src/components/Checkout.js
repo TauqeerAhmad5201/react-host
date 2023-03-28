548328c80605b9060e7e5a552ee58ddf0c75110d
TauqeerAhmad5201 <hellotauqeer@gmail.com>
2023-03-28 18:08:02 +0530

    added second button for array

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -3,14 +3,19 @@ import { useEffect, useState } from "react";
 
 export default function Checkout() {
   const [count, setCount] = useState(0);
+  const [data, setData] = useState(1);
   useEffect(() => {
     document.title = `You clicked ${count} times`;
-  });
+  }, [count]); //now using array, useEffect will only be action for count. 
+
   return (
     <>
+
       <p>You clicked {count} times</p>
       <button onClick={() => setCount(count + 1)}>Click me</button>
-      <button>Done!</button>
+      <button onClick={() => setData(data + 1)}>Update</button>
+      <p>You clicked Click Me {data} times</p>
+
     </>
   );
 }
